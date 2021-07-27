005ff2debfe44684e90cc4700dd1c8d990e042e7
Karlo Marinovic <karlo.marinovic@init.hr>
2021-07-27 16:38:40 +0200

    first

---
+++ simpleweb/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi there, hello');
+});
+
+app.listen(8080, () => console.log('Server is listening on port 8080'));
